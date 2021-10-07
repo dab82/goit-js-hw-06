63c6a1fca669171a2f2e791d5bf12034a5a1520f
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-07 10:30:48 +0300

    2 task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,13 @@
 const categories = document.querySelector("ul#categories");
 console.log(
-	"Number of categories (НОМЕР КАТЕГОРИИ) : ",
+	"Number of categories (НОМЕРА КАТЕГОРИЙ) : ",
 	categories.children.length
 );
 const titleElement = document.querySelectorAll(".item");
 titleElement.forEach((element) => {
-	console.log("Category: ", element.firstElementChild.textContent);
+	console.log("Category/Категория: ", element.firstElementChild.textContent);
 	console.log(
-		"Elements: ",
+		"Elements/Элементы: ",
 		element.firstElementChild.nextElementSibling.children.length
 	);
 });
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+const createLiEl = (array) => {
+	return ingredients.map((ingredient) => {
+		const liEl = document.createElement("li");
+		liEl.classList.add("item");
+		liEl.textContent = ingredient;
+		return liEl;
+	});
+};
+createLiEl(ingredients);
+ulEl.append(...createLiEl(ingredients));
