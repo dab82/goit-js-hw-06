3724f7e780b505154b3aff2c0a5ae0682390ae5d
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-13 21:51:05 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formSubmit = document.querySelector(".login-form");
 
+const onSubmitForm = (event) => {
+	event.preventDefault();
+	const {
+		elements: { email, password },
+	} = event.currentTarget;
+
+	if (email.value === "" || password.value === "") {
+		return alert("Ану не балуйся и заполни все поля!");
+	}
+
+	console.log(`Email: ${email.value}, Password: ${password.value}`);
+	event.currentTarget.reset();
+};
+
+formSubmit.addEventListener("submit", onSubmitForm);
