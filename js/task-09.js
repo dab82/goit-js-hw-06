d6cc02754ba09cb8161e124de292d7c1a15fccf7
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-10-14 13:47:08 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColor = document.querySelector(".change-color");
+const bodyColorText = document.querySelector(".color");
+
+const onBodyColor = () => {
+	document.body.style.backgroundColor = getRandomHexColor();
+	bodyColorText.textContent = `${getRandomHexColor()}`;
+
+	console.log(getRandomHexColor());
+};
+
+changeColor.addEventListener("click", onBodyColor);
